60e6b4aadedc875c1777739ad2f4f1a42dbb6044
bwackwat <bwackwat@github.com>
2017-07-17 11:42:04 -0700

    Threading and queueing clients appears to work, but exiting the application does not...

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#!/bin/python
+#!/bin/python3.4
 
-import socket, time, subprocess, os, traceback
+import socket, time, subprocess, os, traceback, queue, threading
 
 import psycopg2
 
@@ -24,8 +24,8 @@ class DB(object):
 		for table in self.execute("SELECT * FROM pg_tables;"):
 			if table[0] == "public":
 				self.tables[table[1]] = [table_columns[3] for table_columns in self.execute("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '" + table[1] + "';")]
-		for key, value in self.tables.iteritems():
-			print key + ": " + str(value)
+		for key, value in self.tables.items():
+			print(key + ": " + str(value))
 
 class API(object):
 	def __init__(self):
@@ -37,7 +37,7 @@ class API(object):
 		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.server.bind(("", self.port))
-		self.server.listen(10)
+		self.server.listen(util.configuration["number_of_total_connections"])
 
 		self.headers = {
 			"Accept-Ranges": "bytes",
@@ -46,44 +46,47 @@ class API(object):
 
 		self.routes = {}
 
-	def respond(self, status, message, content="", headers={}):
-		header_text = ""
-		for key, value in self.headers.iteritems():
-			header_text += key + ": " + value + "\r\n"
-		for key, value in headers.iteritems():
-			header_text += key + ": " + value + "\r\n"
-		return "HTTP/1.0 " + status + " " + message + "\r\n" + header_text + "\r\n\r\n" + content
+		self.queue = queue.Queue(maxsize=util.configuration["number_of_total_connections"])
+		self.threads = []
 
-	def route(self, method, path, callback):
-		self.routes[method + " " + path] = callback
+		for i in range(util.configuration["number_of_threads"]):
+			thread = threading.Thread(target=self.client_thread)
+			thread.setDaemon(True)
+			thread.start()
+			self.threads.append(thread)
 
-	def start(self):
-		print "API running on port " + str(self.port) + "."
-		print "Routes:"
+	def exit(self):
+		self.running = False
+		for thread in self.threads:
+			thread.join()
+
+	def client_thread(self):
 		client_tries = 0
-		for key, value in self.routes.iteritems():
-			print "\t" + key
 		while self.running:
 			try:
-				client, address = self.server.accept()
-				print address
+				print("Running:" + str(self.running))
+				client, address = self.queue.get()
+				print(address)
 				client.settimeout(10)
-				data = client.recv(4096)
+				data = client.recv(4096).decode("utf-8")
 				if not data:
 					client.close()
-					continue
-				print "RECV: " + data
+					break
+
+				print("RECV: " + data)
 				lines = data.split("\n")
-				print data.split("\r\n\r\n")
-				init = lines[0].split(" ")
+				print(data.split("\r\n\r\n"))
+				http_header = lines[0].split(" ")
 
 				# Default
 				response = self.respond("404", "Not Found", "<h1>404 Not Found</h1>")
-				if len(init) < 3:
+
+				if len(http_header) < 3:
 					response = self.respond("400", "Bad Request", "<h1>400 Bad Request</h1>")
 				else:
-					route = init[0] + " " + init[1]
-					clean_path = self.directory + init[1].replace("..", "")
+					route = http_header[0] + " " + http_header[1]
+					clean_path = self.directory + http_header[1].replace("..", "")
+
 					if route in self.routes:
 						response = self.respond("200", "OK", self.routes[route]())
 					elif os.path.isdir(clean_path):
@@ -91,12 +94,41 @@ class API(object):
 					if os.path.isfile(clean_path):
 						with open(clean_path, "r") as f:
 							response = self.respond("200", "OK", f.read())
-				client.send(response)
+
+				client.send(response.encode("utf-8"))
 				client.close()
 			except:
 				client_tries += 1
 				if client_tries > 3:
-					self.running = False
+					break
+				traceback.print_exc()
+
+	def respond(self, status, message, content="", headers={}):
+		header_text = ""
+		for key, value in self.headers.items():
+			header_text += key + ": " + value + "\r\n"
+		for key, value in headers.items():
+			header_text += key + ": " + value + "\r\n"
+		return "HTTP/1.1 " + str(status) + " " + message + "\r\n" + header_text + "\r\n\r\n" + content
+
+	def route(self, method, path, callback):
+		self.routes[method + " " + path] = callback
+
+	def start(self):
+		print("API running on port " + str(self.port) + ".")
+		print("Routes:")
+		for key, value in self.routes.items():
+			print("\t" + key)
+
+		server_tries = 0
+		while self.running:
+			try:
+				print(self.running)
+				self.queue.put(self.server.accept())
+			except:
+				server_tries += 1
+				if server_tries > 3:
+					self.exit()
 				traceback.print_exc()
 
 if __name__ == "__main__":
@@ -105,18 +137,17 @@ if __name__ == "__main__":
 	def procs():
 		p = subprocess.Popen("ps -aux", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		o, e = p.communicate()
-		return o
+		return o.decode("utf-8")
 
 	api.route("GET", "/procs", procs)
 
-	def die(content):
-		if content == api.password:
-			api.running = False
-			return "Goodbye!"
-		return "I'm alive!"
+	def die():
+		api.running = False
+		return "<h1>Goodbye!</h1>"
 
 	api.route("GET", "/die", die)
 
 	db = DB()
 
 	api.start()
+	api.queue.join()
